bb60b19f35ffb900164483d24b30b203a7a73889
Nutnarong <35445939+hnutnrong@users.noreply.github.com>
2020-02-17 15:58:31 +0700

    [INSERT] controller พื้นฐาน

---
+++ laravel-project/resources/views/contact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ติดต่อเรา</h1>
+
+
+    <a href="/">กลับหน้าหลัก</a>
+
+     <br>
+
+    <a href="/about">เกี่ยวกับเรา</a>
+</body>
+</html>
--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/about',function(){
+    return view('about');
 });
+
+Route::get('/contact',function(){
+    return view('contact');
+});
+
+Route::get('product','ProductController@index');
+
+
+
+
+
+
+
+
+
+Route::get('/','HomeController@index');
+
+Route::get('product/{id}','HomeController@product');
+
+
